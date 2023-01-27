c3b955efde9c3ee004372cb307ec28f2728a7595
konstantin <mom.konstantin@gmail.com>
2023-01-27 23:42:19 +0300

    Init

---
+++ src/com/hw/array1/Main.java
@@ -0,0 +1,10 @@
+package com.hw.array1;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача1");
+    }
+}
