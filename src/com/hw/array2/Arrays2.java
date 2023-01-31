73cc0e46467d997acb34b7767f96e8e794506781
konstantin <mom.konstantin@gmail.com>
2023-01-31 12:25:12 +0300

    Init

---
+++ src/com/hw/array2/Arrays2.java
@@ -0,0 +1,11 @@
+package com.hw.array2;
+
+public class Arrays2 {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+    }
+}
